829cd134e98a7b3c6307278e324b260982bed663
Gokmen Gorgen <gokmen@alageek.com>
2012-05-30 08:06:57 +0300

    added setting model.

--- radpress/context_processors.py
+++ radpress/context_processors.py
@@ -1,11 +1,19 @@
-from radpress import settings
+from radpress.settings import DEFAULT_SETTINGS
+from radpress.models import Setting
 
 
 def context_data(request):
-    data = {
-        'RADPRESS_TITLE': settings.TITLE,
-        'RADPRESS_DESCRIPTION': settings.DESCRIPTION,
-        'RADPRESS_GA_CODE': settings.GA_CODE
-    }
+    data = {}
+    current_settings = Setting.objects.get_current_settings()
+    for setting in current_settings:
+        if setting.endswith('id'):
+           continue
+
+        data_key = 'RADPRESS_%s' % setting.upper()
+        data_value = current_settings.get(setting)
+        if not data_value:
+            data_value = DEFAULT_SETTINGS.get(setting)
+
+        data.update({data_key: data_value})
 
     return data
--- radpress/settings.py
+++ radpress/settings.py
@@ -1,6 +1,7 @@
-from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
-TITLE = getattr(settings, 'RADPRESS_TITLE', 'RADPRESS')
-DESCRIPTION = getattr(
-    settings, 'RADPRESS_DESCRIPTION', 'Simple django blog engine.')
-GA_CODE = getattr(settings, 'RADPRESS_GA_CODE', None)
+DEFAULT_SETTINGS = {
+    'title': 'RADPRESS',
+    'description': _("Simple Django Blog Engine..")
+}
+
