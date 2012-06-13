f9a985598c3c172ba3eaacecef7c7775cd60d87b
Gokmen Gorgen <gokmen@alageek.com>
2012-06-13 23:11:54 +0300

    update migration scripts. fix disqus setting of demo.

--- radpress/migrations/0001_initial.py
+++ radpress/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(SchemaMigration):
         # Adding model 'Article'
         db.create_table('radpress_article', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=500)),
             ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
             ('content', self.gf('django.db.models.fields.TextField')()),
             ('content_body', self.gf('django.db.models.fields.TextField')()),
@@ -29,18 +29,18 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal('radpress', ['Article'])
 
-        # Adding M2M table for field tags on 'Article'
-        db.create_table('radpress_article_tags', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('article', models.ForeignKey(orm['radpress.article'], null=False)),
-            ('tag', models.ForeignKey(orm['radpress.tag'], null=False))
+        # Adding model 'ArticleTag'
+        db.create_table('radpress_articletag', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['radpress.Tag'])),
+            ('article', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['radpress.Article'])),
         ))
-        db.create_unique('radpress_article_tags', ['article_id', 'tag_id'])
+        db.send_create_signal('radpress', ['ArticleTag'])
 
         # Adding model 'Page'
         db.create_table('radpress_page', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=500)),
             ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
             ('content', self.gf('django.db.models.fields.TextField')()),
             ('content_body', self.gf('django.db.models.fields.TextField')()),
@@ -50,32 +50,21 @@ class Migration(SchemaMigration):
         ))
         db.send_create_signal('radpress', ['Page'])
 
-        # Adding model 'Setting'
-        db.create_table('radpress_setting', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('site', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['sites.Site'], unique=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
-            ('description', self.gf('django.db.models.fields.TextField')(max_length=256, blank=True)),
-            ('ga_code', self.gf('django.db.models.fields.CharField')(max_length=32, blank=True)),
-        ))
-        db.send_create_signal('radpress', ['Setting'])
-
         # Adding model 'Menu'
         db.create_table('radpress_menu', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('setting', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['radpress.Setting'])),
             ('order', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=3)),
             ('page', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['radpress.Page'], unique=True)),
         ))
         db.send_create_signal('radpress', ['Menu'])
 
-        # Adding unique constraint on 'Menu', fields ['setting', 'order', 'page']
-        db.create_unique('radpress_menu', ['setting_id', 'order', 'page_id'])
+        # Adding unique constraint on 'Menu', fields ['order', 'page']
+        db.create_unique('radpress_menu', ['order', 'page_id'])
 
 
     def backwards(self, orm):
-        # Removing unique constraint on 'Menu', fields ['setting', 'order', 'page']
-        db.delete_unique('radpress_menu', ['setting_id', 'order', 'page_id'])
+        # Removing unique constraint on 'Menu', fields ['order', 'page']
+        db.delete_unique('radpress_menu', ['order', 'page_id'])
 
         # Deleting model 'Tag'
         db.delete_table('radpress_tag')
@@ -83,14 +72,11 @@ class Migration(SchemaMigration):
         # Deleting model 'Article'
         db.delete_table('radpress_article')
 
-        # Removing M2M table for field tags on 'Article'
-        db.delete_table('radpress_article_tags')
+        # Deleting model 'ArticleTag'
+        db.delete_table('radpress_articletag')
 
         # Deleting model 'Page'
         db.delete_table('radpress_page')
-
-        # Deleting model 'Setting'
-        db.delete_table('radpress_setting')
 
         # Deleting model 'Menu'
         db.delete_table('radpress_menu')
@@ -98,54 +84,45 @@ class Migration(SchemaMigration):
 
     models = {
         'radpress.article': {
-            'Meta': {'ordering': "('-created_at', 'updated_at')", 'object_name': 'Article'},
+            'Meta': {'ordering': "('-created_at', '-updated_at')", 'object_name': 'Article'},
             'content': ('django.db.models.fields.TextField', [], {}),
             'content_body': ('django.db.models.fields.TextField', [], {}),
             'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
-            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['radpress.Tag']", 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'tags': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['radpress.Tag']", 'null': 'True', 'through': "orm['radpress.ArticleTag']", 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
             'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        'radpress.articletag': {
+            'Meta': {'object_name': 'ArticleTag'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['radpress.Article']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['radpress.Tag']"})
         },
         'radpress.menu': {
-            'Meta': {'unique_together': "(('setting', 'order', 'page'),)", 'object_name': 'Menu'},
+            'Meta': {'unique_together': "(('order', 'page'),)", 'object_name': 'Menu'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'order': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '3'}),
-            'page': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['radpress.Page']", 'unique': 'True'}),
-            'setting': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['radpress.Setting']"})
+            'page': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['radpress.Page']", 'unique': 'True'})
         },
         'radpress.page': {
-            'Meta': {'ordering': "('-created_at', 'updated_at')", 'object_name': 'Page'},
+            'Meta': {'ordering': "('-created_at', '-updated_at')", 'object_name': 'Page'},
             'content': ('django.db.models.fields.TextField', [], {}),
             'content_body': ('django.db.models.fields.TextField', [], {}),
             'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_published': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
             'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
-        },
-        'radpress.setting': {
-            'Meta': {'object_name': 'Setting'},
-            'description': ('django.db.models.fields.TextField', [], {'max_length': '256', 'blank': 'True'}),
-            'ga_code': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'site': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['sites.Site']", 'unique': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
         },
         'radpress.tag': {
             'Meta': {'object_name': 'Tag'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
-        },
-        'sites.site': {
-            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
-            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
     }
 
